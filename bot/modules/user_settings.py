a9ab098dd0a95b598a0d3042f7562a341507f781
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-02-18 19:33:09 +0700

    Update button text and style in user settings

--- bot/modules/user_settings.py
+++ bot/modules/user_settings.py
@@ -585,7 +585,9 @@ async def uset_qb_instr_handler(client, query):
         "Contoh:\n"
         "<code>/qobuz_login 123456 r5T6y7U8...</code>"
     )
-    buttons = [[InlineKeyboardButton("🔙 Kembali", callback_data="uset_qb_auth")]]
+    style=ButtonStyle.PRIMARY
+    buttons = [[InlineKeyboardButton("🔙 Back", callback_data="uset_qb_auth", style=ButtonStyle.PRIMARY)]]
+    
     await edit_message(query.message, text, markup=InlineKeyboardMarkup(buttons))
 
 
@@ -664,7 +666,9 @@ async def uset_dz_remove_handler(client, query):
 @Client.on_callback_query(filters.regex("^uset_dz_instr"))
 async def uset_dz_instr_handler(client, query):
     text = "Ketik: <code>/deezer_login arl_anda_disini</code>"
-    buttons = [[InlineKeyboardButton("🔙 Kembali", callback_data="uset_dz_auth")]]
+    style=ButtonStyle.PRIMARY
+    buttons = [[InlineKeyboardButton("🔙 Kembali", callback_data="uset_dz_auth", style=ButtonStyle.PRIMARY)]]
+    
     await edit_message(query.message, text, markup=InlineKeyboardMarkup(buttons))
 
 
