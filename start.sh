e574500c6b464a3e28060db2a23e36017ff60391
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-03-01 20:43:50 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+# Menjalankan daemon aria2c di background dengan mode RPC aktif
+aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port=6800 --max-connection-per-server=10 --rpc-max-request-size=1024M --seed-time=0 &
+
+# Menjalankan bot utama
 python3 -m bot
