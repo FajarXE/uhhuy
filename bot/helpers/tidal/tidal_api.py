1b3c526571629e2c0803c44146647be2a72ed08c
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-03-04 02:13:52 +0700

    Update tidal_api.py

--- bot/helpers/tidal/tidal_api.py
+++ bot/helpers/tidal/tidal_api.py
@@ -176,10 +176,10 @@ class TidalApi:
 
     async def get_stream_url(self, track_id, quality, session):
         return await self._get('tracks/' + str(track_id) + '/playbackinfopostpaywall/v4', {
-            'playbackmode': 'STREAM',
+            'playbackmode': 'DOWNLOAD', # <--- GANTI 'STREAM' MENJADI 'DOWNLOAD' DI SINI
             'assetpresentation': 'FULL',
-            'audioquality': quality
-            # [FIX] Baris 'prefetch': 'false' dihapus total di sini agar Tidal CDN melepaskan limit kecepatan!
+            'audioquality': quality,
+            'prefetch': 'false'
         },
         session)
 
