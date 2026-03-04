12a2e08ceaa890a988ee3b23a117cf479495e5d0
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-03-04 14:09:20 +0700

    Update aria2_helper.py

--- bot/helpers/aria2_helper.py
+++ bot/helpers/aria2_helper.py
@@ -95,7 +95,7 @@ async def aria2_download(url, filepath, details=None):
                         
                 # --- [FIX LATENSI ARIA2] ---
                 # Ganti dari 2 detik menjadi 0.5 detik agar respon Aria2 pada file kecil menjadi super kilat!
-                await asyncio.sleep(0.5) 
+                await asyncio.sleep(0.1) 
  
     except Exception as e:
         LOGGER.error(f"Aria2 RPC Exception: {e}")
