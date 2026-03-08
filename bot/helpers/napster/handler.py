a542a441b65c770e36db3c1464747dfe0b3ee2a7
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-03-08 21:08:23 +0700

    Update handler.py

--- bot/helpers/napster/handler.py
+++ bot/helpers/napster/handler.py
@@ -221,9 +221,7 @@ async def start_album(album_id: str, user: dict, upload=True):
     # --- BATAS MODIFIKASI ---
 
     if album_zip: 
-        await edit_message(user['bot_msg'], f"Menyiapkan {album_meta['totaltracks']} lagu menjadi .zip...")
         album_meta['zip_path'] = await zip_handler(album_meta['folderpath'])
 
     if upload:
-        await edit_message(user['bot_msg'], lang.s.UPLOADING)
         await album_upload(album_meta, user)
