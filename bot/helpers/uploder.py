4f8160b204d08f95e09edbd43cb1b7cfedbb75b0
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-03-08 17:30:57 +0700

    Update uploder.py

--- bot/helpers/uploder.py
+++ bot/helpers/uploder.py
@@ -72,15 +72,15 @@ async def upload_to_cloud_handler(filepath, user, metadata, mode):
     details = None
     if 'bot_msg' in user:
         import time, hashlib
-        # Buat ID Cancel acak & unik khusus untuk unggahan ini
-        task_id = hashlib.md5(str(time.time()).encode()).hexdigest()[:16]
+        # --- [TRANSISI MULUS] GUNAKAN ID PESAN ASLI ---
+        task_id = hashlib.md5(str(user['bot_msg'].id).encode()).hexdigest()[:16]
         details = {
             'msg': user['bot_msg'],
             'title': metadata.get('title', 'Unknown'),
             'type': metadata.get('type', 'Task').capitalize(),
-            'action': 'Upload', # <-- Mengubah "Download Album" menjadi "Upload Album"
-            'machine': 'AIOHTTP Streaming', # <-- Nama Mesin Asli
-            'task_id': task_id # <-- Agar ID Cancel menjadi unik
+            'action': 'Upload', 
+            'machine': 'AIOHTTP Streaming', 
+            'task_id': task_id 
         }
     # ------------------------------------
 
