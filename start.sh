dbe6ed99279e98b0d20fb3f8300db16bbf9ee97f
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-03-08 15:25:08 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Menjalankan daemon aria2c di background dengan mode RPC aktif
-aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port=6800 --max-connection-per-server=10 --rpc-max-request-size=1024M --seed-time=0 &
+# Menjalankan daemon aria2c di background dengan mode RPC aktif dan Max Speed
+aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port=6800 -j 20 --max-connection-per-server=16 --split=16 --min-split-size=1M --continue=true --allow-overwrite=true --rpc-max-request-size=1024M --seed-time=0 &
 
 # Menjalankan bot utama
 python3 -m bot
