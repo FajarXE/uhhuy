c6dcbb0ef0617126076615198e48b5d448216729
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-03-08 20:53:28 +0700

    Update handler.py

--- bot/helpers/highresaudio/handler.py
+++ bot/helpers/highresaudio/handler.py
@@ -217,11 +217,9 @@ async def start_album(album_url: str, user: dict, upload=True):
     playlist_zip, album_zip, artist_zip, art_poster = fetch_zip_settings(user)
 
     if album_zip: 
-        await edit_message(user['bot_msg'], f"Menyiapkan {album_meta['totaltracks']} lagu menjadi .zip...")
         album_meta['zip_path'] = await zip_handler(album_meta['folderpath'])
 
     if upload:
-        await edit_message(user['bot_msg'], lang.s.UPLOADING)
 
         if 'zip_path' not in album_meta and booklet_path and os.path.exists(booklet_path):
             try:
