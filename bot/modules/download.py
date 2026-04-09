a8a958daceef91fa8332686bc41ea5c34b5fe270
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-04-09 12:43:08 +0700

    Update download.py
    Auto detect link

--- bot/modules/download.py
+++ bot/modules/download.py
@@ -462,7 +462,7 @@ async def run_download_task(link: str, user: dict):
                 pass
 
 
-@Client.on_message(filters.command(CMD.DOWNLOAD))
+@Client.on_message(filters.command(CMD.DOWNLOAD) | filters.regex(r"https?://[^\s]+"))
 async def download_track(c, msg:Message):
     if await check_user(msg=msg):
         
