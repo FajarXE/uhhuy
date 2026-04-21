2222112fa440cc435b7b2f311c2a9c3b342dc37d
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-04-21 16:21:50 +0700

    Refactor MongoDB class to use AsyncIOMotorClient

--- bot/helpers/database/mongo_async.py
+++ bot/helpers/database/mongo_async.py
@@ -1,24 +1,25 @@
-from async_pymongo import AsyncClient
+import logging
+import traceback
+from motor.motor_asyncio import AsyncIOMotorClient
 from config import Config
-
-import logging, traceback
 
 class MongoDB:
     """
-      An Async Database
+      An Async Database using Motor
     """
     
     def __init__(self) -> None:
-        self.db = AsyncClient(Config.DATABASE_URL)
+        # Mengganti AsyncClient dengan AsyncIOMotorClient
+        self.db = AsyncIOMotorClient(Config.DATABASE_URL)
         self.client = self.db[Config.BOT_USERNAME]
     
     async def initialize_users(self) -> dict:
         exists = await self.db[Config.BOT_USERNAME].users.find_one({})
         if exists:
             user_data = {} 
-            rows = self.db[Config.BOT_USERNAME].users.find({})
-            # Return User data
-            async for row in rows:
+            # Menggunakan AsyncIOMotorCursor
+            cursor = self.db[Config.BOT_USERNAME].users.find({})
+            async for row in cursor:
                 uid = row["_id"]
                 del row["_id"]
                 user_data[uid] = row
@@ -27,30 +28,52 @@ class MongoDB:
     
     async def authorize_chats(self, user_id: int, remove=False) -> bool:
         if remove:
-            ret = await self.client.music.update_one({"_id": Config.BOT_USERNAME}, {"pull": {"AUTH_CHATS": user_id}}, upsert=True)
+            # Memperbaiki typo "pull" menjadi "$pull"
+            ret = await self.client.music.update_one(
+                {"_id": Config.BOT_USERNAME}, 
+                {"$pull": {"AUTH_CHATS": user_id}}, 
+                upsert=True
+            )
             return bool(ret)
         
-        ret = await self.client.music.update_one({"_id": Config.BOT_USERNAME}, {"$addToSet": {"AUTH_CHATS": user_id}}, upsert=True)
-        
+        ret = await self.client.music.update_one(
+            {"_id": Config.BOT_USERNAME}, 
+            {"$addToSet": {"AUTH_CHATS": user_id}}, 
+            upsert=True
+        )
         return bool(ret)
     
     async def authorize_users(self, user_id: int, remove=False) -> bool:
         if remove:
-            ret = await self.client.music.update_one({"_id": Config.BOT_USERNAME}, {"pull": {"AUTH_USERS": user_id}}, upsert=True)
+            # Memperbaiki typo "pull" menjadi "$pull"
+            ret = await self.client.music.update_one(
+                {"_id": Config.BOT_USERNAME}, 
+                {"$pull": {"AUTH_USERS": user_id}}, 
+                upsert=True
+            )
             return bool(ret)
         
-        ret = await self.client.music.update_one({"_id": Config.BOT_USERNAME}, {"$addToSet": {"AUTH_USERS": user_id}}, upsert=True)
-        
+        ret = await self.client.music.update_one(
+            {"_id": Config.BOT_USERNAME}, 
+            {"$addToSet": {"AUTH_USERS": user_id}}, 
+            upsert=True
+        )
         return bool(ret)
     
     async def set_variable(self, key: str, value: str|bool|int|None) -> bool:
-        ret = await self.client.music.update_one({"_id": Config.BOT_USERNAME}, {"$set": {key: value}}, upsert=True)
+        ret = await self.client.music.update_one(
+            {"_id": Config.BOT_USERNAME}, 
+            {"$set": {key: value}}, 
+            upsert=True
+        )
         return bool(ret)
     
     async def get_variable(self, query: None|dict = None) -> dict:
         if not query:
             query = {}
-        data_list = [i async for i in self.client.music.find(query)]
+            
+        # Motor menggunakan to_list() untuk mengekstrak seluruh data cursor secara efisien
+        data_list = await self.client.music.find(query).to_list(length=None)
         data_dict = {}
         
         for data in data_list:
@@ -59,10 +82,16 @@ class MongoDB:
             data_dict.pop("_id")
         return data_dict
 
-    async def save_user_settings(self, user_id: int=0, data: dict={}) -> None:
+    async def save_user_settings(self, user_id: int=0, data: dict=None) -> None:
+        if data is None:
+            data = {}
         user_id = int(user_id) if isinstance(user_id, str) else user_id
         try:
-            await self.db[Config.BOT_USERNAME].users.update_one({"_id": user_id}, {"$set": data}, upsert=True)
+            await self.db[Config.BOT_USERNAME].users.update_one(
+                {"_id": user_id}, 
+                {"$set": data}, 
+                upsert=True
+            )
         except Exception:
             logging.info(traceback.format_exc())
 
