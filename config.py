851abbbf8bf91077879de00669130f993756a47f
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-04-22 10:49:14 +0700

    Remove Spotify client ID and secret configuration
    Removed Spotify configuration variables from config.py.

--- config.py
+++ config.py
@@ -449,19 +449,6 @@ class Config:
 # --- BATAS TAMBAHAN ---
 
 #--------------------
-# SPOTIFY
-#--------------------
-    # Default Client ID ini adalah ID umum, tapi disarankan pakai punya sendiri
-    # Daftar di developer.spotify.com
-    SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "")
-    SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "")
-    
-    # Variabel ini akan menyimpan JSON Token Login agar tidak hilang saat Restart di Render
-    SPOTIFY_CREDENTIALS_JSON = getenv("SPOTIFY_CREDENTIALS_JSON", None)
-#--------------------
-# --- BATAS TAMBAHAN ---
-
-#--------------------
 # RENDER MANAGEMENT
 #--------------------
     # API Key dari Dashboard Render -> Account Settings -> API Keys
