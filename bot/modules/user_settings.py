e90f6118259231510185411695ec0e345663828f
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-05-08 01:44:04 +0700

    Fix syntax error in user_settings.py

--- bot/modules/user_settings.py
+++ bot/modules/user_settings.py
@@ -1610,7 +1610,8 @@ async def uset_cb(client, query, datatype=""):
             "MHM1": "3D (mhm1)",
             "UHD": "UHD (Hi-Res)",
             "HD": "HD (Lossless)",
-            "SD": "SD (Opus)"
+            "SD": "SD (Opus)",
+            "LD": "LD (Low Def)"
         }
         
         # Pengecekan manager bersifat opsional di sini agar menu tidak blank
