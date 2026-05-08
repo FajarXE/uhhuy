d58b2634e1830d84457151521a46e4b80c5dced5
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-05-08 15:56:51 +0700

    Update audio quality options for Amazon Music

--- bot/modules/user_settings.py
+++ bot/modules/user_settings.py
@@ -1606,12 +1606,12 @@ async def uset_cb(client, query, datatype=""):
         quality = {
             "AC-4": "AC-4 (Dolby Atmos)",
             "EC-3": "EC-3 (Dolby Digital Plus)",
-            "MHA1": "3D (mha1)",
-            "MHM1": "3D (mhm1)",
+            "MHA1": "MPEG-H 3D (mha1)",
+            "MHM1": "MPEG-H 3D (mhm1)",
             "UHD": "UHD (Hi-Res)",
             "HD": "HD (Lossless)",
-            "SD": "SD (Opus)",
-            "LD": "LD (Low Def)"
+            "SD": "SD (Opus - High)",
+            "LD": "LD (Opus - Med/Low)"
         }
         
         # Pengecekan manager bersifat opsional di sini agar menu tidak blank
