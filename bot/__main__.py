b5593a30b63d7e97ab81087c11832d174c580e7b
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-05-13 23:05:19 +0700

    Add genie_manager to import and manager lists

--- bot/__main__.py
+++ bot/__main__.py
@@ -64,6 +64,7 @@ livephish_manager = safe_import('bot.helpers.livephish.manager', 'livephish_mana
 beatstars_manager = safe_import('bot.helpers.beatstars.manager', 'beatstars_manager')
 khinsider_manager = safe_import('bot.helpers.khinsider.manager', 'khinsider_manager')
 amazon_manager = safe_import('bot.helpers.amazon.manager', 'amazon_manager')
+genie_manager = safe_import('bot.helpers.genie.manager', 'genie_manager')
 
 
 # --- [DEBUG] EXCEPTION HANDLER ---
@@ -174,7 +175,7 @@ async def start_services():
         (highresaudio_manager, "HIGHRESAUDIO"), (moov_manager, "Moov"),
         (jiosaavn_manager, "JioSaavn"), (gaana_manager, "Gaana"), 
         (bandcamp_manager, "Bandcamp"), (livephish_manager, "LivePhish"), 
-        (beatstars_manager, "BeatStars"), (khinsider_manager, "Khinsider"), (amazon_manager, "Amazon Music")
+        (beatstars_manager, "BeatStars"), (khinsider_manager, "Khinsider"), (amazon_manager, "Amazon Music"), (genie_manager, "Genie")
     ]
 
     for mgr, name in managers:
@@ -232,7 +233,7 @@ async def shutdown_all_services():
     managers_list = [
         deezer_manager, beatport_manager, tidal_manager, kkbox_manager,
         beatsource_manager, soundcloud_manager, idagio_manager, nugs_manager, bugs_manager, highresaudio_manager, moov_manager,
-        jiosaavn_manager, gaana_manager, bandcamp_manager, livephish_manager, beatstars_manager, khinsider_manager, amazon_manager
+        jiosaavn_manager, gaana_manager, bandcamp_manager, livephish_manager, beatstars_manager, khinsider_manager, amazon_manager, genie_manager
     ]
     for mgr in managers_list:
         if mgr and hasattr(mgr, 'shutdown'):
