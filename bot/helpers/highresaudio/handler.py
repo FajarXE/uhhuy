cdf18ff0a63d824c8c5b624a47de09fa393819ad
Fajar M <129875302+fajarmaulan4m@users.noreply.github.com>
2026-05-20 22:32:14 +0700

    Reduce concurrency limit for task execution

--- bot/helpers/highresaudio/handler.py
+++ bot/helpers/highresaudio/handler.py
@@ -271,7 +271,7 @@ async def start_album(album_url: str, user: dict, upload=True):
         'type': album_meta['type']
     }
     
-    task_results = await run_concurrent_tasks(tasks, update_details, limit=8)
+    task_results = await run_concurrent_tasks(tasks, update_details, limit=4)
     
     successful_tracks = [album_meta['tracks'][i] for i, result in enumerate(task_results) if result]
     album_meta['tracks'] = successful_tracks
