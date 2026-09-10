607c3d3254f4bd5cdbcb80b484c46f499015f790
Fajar Maulana <106910046+FajarXE@users.noreply.github.com>
2026-09-10 17:25:57 +0700

    Adjust aria2c connection and split parameters
    Reduced max connections and split settings for aria2c.

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Menjalankan daemon aria2c secara native (--daemon=true) dan listen ke semua interface (--rpc-listen-all=true)
-aria2c --daemon=true --enable-rpc --rpc-listen-all=true --rpc-listen-port=6800 -j 20 --max-connection-per-server=16 --split=16 --min-split-size=1M --continue=true --allow-overwrite=true --rpc-max-request-size=1024M --seed-time=0
+aria2c --daemon=true --enable-rpc --rpc-listen-all=true --rpc-listen-port=6800 -j 20 --max-connection-per-server=8 --split=8 --min-split-size=5M --continue=true --allow-overwrite=true --rpc-max-request-size=1024M --seed-time=0
 
 # Menjalankan bot utama
 python3 -m bot
