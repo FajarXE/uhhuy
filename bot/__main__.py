e0799831307d59d91957d9a4e74fb754977a7664
Fajar Maulana <106910046+FajarXE@users.noreply.github.com>
2026-09-23 23:45:43 +0700

    Remove khinsider_manager from bot managers
    Removed khinsider_manager from the list of managers.

--- bot/__main__.py
+++ bot/__main__.py
@@ -61,7 +61,6 @@ gaana_manager = safe_import('bot.helpers.gaana.manager', 'gaana_manager')
 bandcamp_manager = safe_import('bot.helpers.bandcamp.manager', 'bandcamp_manager')
 livephish_manager = safe_import('bot.helpers.livephish.manager', 'livephish_manager')
 beatstars_manager = safe_import('bot.helpers.beatstars.manager', 'beatstars_manager')
-khinsider_manager = safe_import('bot.helpers.khinsider.manager', 'khinsider_manager')
 amazon_manager = safe_import('bot.helpers.amazon.manager', 'amazon_manager')
 genie_manager = safe_import('bot.helpers.genie.manager', 'genie_manager')
 
@@ -180,7 +179,7 @@ async def start_services():
         (highresaudio_manager, "HIGHRESAUDIO"), (moov_manager, "Moov"),
         (jiosaavn_manager, "JioSaavn"), (gaana_manager, "Gaana"), 
         (bandcamp_manager, "Bandcamp"), (livephish_manager, "LivePhish"), 
-        (beatstars_manager, "BeatStars"), (khinsider_manager, "Khinsider"), (amazon_manager, "Amazon Music"), (genie_manager, "Genie")
+        (beatstars_manager, "BeatStars"), (amazon_manager, "Amazon Music"), (genie_manager, "Genie")
     ]
 
     for mgr, name in managers:
@@ -310,7 +309,7 @@ async def shutdown_all_services():
     managers_list = [
         deezer_manager, beatport_manager, tidal_manager, kkbox_manager,
         soundcloud_manager, idagio_manager, nugs_manager, bugs_manager, highresaudio_manager, moov_manager,
-        jiosaavn_manager, gaana_manager, bandcamp_manager, livephish_manager, beatstars_manager, khinsider_manager, amazon_manager, genie_manager
+        jiosaavn_manager, gaana_manager, bandcamp_manager, livephish_manager, beatstars_manager, amazon_manager, genie_manager
     ]
     for mgr in managers_list:
         if mgr and hasattr(mgr, 'shutdown'):
