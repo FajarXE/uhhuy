73beb067d2a1ccf63bc931fb305e45a7f45e570f
Fajar Maulana <106910046+FajarXE@users.noreply.github.com>
2026-09-23 23:27:40 +0700

    Add KHINSIDER proxy configuration handling

--- config.py
+++ config.py
@@ -460,6 +460,15 @@ class Config:
         logging.info("Tidak ada proxy untuk Genie. Menggunakan koneksi langsung (Raw IP).")
 #--------------------
 # --- BATAS TAMBAHAN ---
+
+#--------------------    
+# KHINSIDER
+#--------------------
+    KHINSIDER_PROXY = getenv("KHINSIDER_PROXY", None)
+    if KHINSIDER_PROXY and KHINSIDER_PROXY.startswith("socks5h://"):
+        KHINSIDER_PROXY = KHINSIDER_PROXY.replace("socks5h://", "socks5://")
+#--------------------
+# --- BATAS TAMBAHAN ---
     
 #--------------------
 # RENDER MANAGEMENT
